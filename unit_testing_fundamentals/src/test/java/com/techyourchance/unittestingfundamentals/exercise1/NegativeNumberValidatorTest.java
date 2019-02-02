77235d08ce5d5ac73356b78d0b8aa653f0502bcc
Josip Komparić <komparic.josip@gmail.com>
2019-02-02 21:07:08 +0100

    exercise 1

--- unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise1/NegativeNumberValidatorTest.java
+++ unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise1/NegativeNumberValidatorTest.java
@@ -1,5 +1,32 @@
 package com.techyourchance.unittestingfundamentals.exercise1;
 
-public class NegativeNumberValidatorTest {
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
+public class NegativeNumberValidatorTest {
+    NegativeNumberValidator SUT;
+
+    @Before
+    public void setup(){
+        SUT = new NegativeNumberValidator();
+    }
+
+    @Test
+    public void testNegatives(){
+        Assert.assertTrue(SUT.isNegative(-1));
+        Assert.assertTrue(SUT.isNegative(-1000));
+        Assert.assertTrue(SUT.isNegative(-99999));
+    }
+
+    @Test
+    public void testZero(){
+        Assert.assertFalse(SUT.isNegative(0));
+    }
+
+    @Test
+    public void testPositive(){
+        Assert.assertFalse(SUT.isNegative(1));
+        Assert.assertFalse(SUT.isNegative(99999));
+    }
 }
